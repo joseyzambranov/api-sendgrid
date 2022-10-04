951637d24eb76d0a3eb1d1f959afdd8a4456b6d2
jose zambrano <jose.zambrano@sinapsiscorp.com>
2022-10-03 19:10:12 -0500

    modyfing the insert month and year

--- index.js
+++ index.js
@@ -3,49 +3,53 @@ require('dotenv').config()
 const clientSendGrid = require('@sendgrid/client');
 clientSendGrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-const ObjectID = require('mongodb').ObjectID;
+const ObjectId = require('mongodb').ObjectId;
 const uri =process.env.DB_MONGO_CONNECTION_STRING;
 const client = new MongoClient(uri);
-const myObjectId = ObjectID("633764cb7d21000072003da2")
+const myObjectId = ObjectId("633764cb7d21000072003da2")
 const dbName = 'email';
-const request = { url: `/v3/validations/email`, method: 'POST', body: { "email": "exampleexample.com","source": "signup" }}
+const request = { url: `/v3/validations/email`, method: 'POST', body: { "email": "example@example.com","source": "signup" }}
 
 async function main() {
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
   const collection = db.collection('counter');
-  const findResult = await collection.find({_id: myObjectId}).toArray();
-  const dateCounter = findResult[0].process_date
-  const countCounter = findResult[0].count
+  //const findResult = await collection.find({_id: myObjectId}).toArray();
+  const findResult = await collection.find({ process_date:{"$gt": new Date()} });
+  //const dateCounter = findResult.process_date
+  //const countCounter = findResult.counts
 
-  if(new Date(dateCounter).getUTCMonth() == new Date().getUTCMonth()){
-       if(countCounter>=5000){
+ // if(new Date(findResult.process_date).getUTCMonth() == new Date().getUTCMonth()){
+  console.log("date mongo "+ new Date(findResult.process_date).getMonth())
+  console.log("date now "+ new Date().getUTCMonth())
+
+  if(new Date(findResult.process_date).getMonth() == new Date().getUTCMonth()){
+       if(findResult.counts>=5000){
         return "no se puede hacer peticiones porque sobrepasa la cantidad permitida"
         }
         else{
-         await collection.updateOne({_id: myObjectId}, { $set: { count: countCounter+1,"process_date": new Date() }  });
+         await collection.updateOne({_id: findResult._id}, { $set: { counts: findResult.counts + 1 } ,$setOnInsert: {  process_date: new Date() } });
          await clientSendGrid.request(request)
           .then(([response, body]) => {
             if(response.body.result.verdict == "Invalid"){
-                console.log("hay "+countCounter+" count y el correo es " + response.body.result.verdict + " el proceso se detiene");
+                console.log(`hay ${findResult.counts} count y el correo es ${response.body.result.verdict} el proceso se detiene y se incrementa 1 quedando el count = ${findResult.counts + 1}`);
             }else{
-                console.log("hay "+countCounter+" count y el correo es " + response.body.result.verdict)
+                console.log(`hay ${findResult.counts} count y el correo es ${response.body.result.verdict} y se incrementa 1 quedando el count = ${findResult.counts + 1}`)
             }
-            
           })
           .catch(error => {
             console.error(error);
           });
         }
   }else{
-    await collection.updateOne({_id: myObjectId}, { $set: { count: 0,"process_date": new Date() }  });
+    await collection.insertOne( { counts: 1 ,process_date: new Date() }  );
          await clientSendGrid.request(request)
           .then(([response, body]) => {
             if(response.body.result.verdict == "Invalid"){
-                console.log("empeiza un numevo mes y el count esta en 0, el correo es " + response.body.result.verdict + " el proceso se detiene");
+                console.log(`comienza un nuevo mes y el count se reseteo a 1, el correo es ${response.body.result.verdict} el proceso se detiene y se incrementa 1 quedando el count = ${findResult.counts + 1}`);
             }else{
-                console.log("empeiza un numevo mes y el count esta en 0, el correo es " + response.body.result.verdict)
+                console.log(`comienza un nuevo mes y el count se reseteo a 1, el correo es ${response.body.result.verdict} y se incrementa 1 quedando el count = ${findResult.counts + 1}`)
             }
           })
           .catch(error => {
